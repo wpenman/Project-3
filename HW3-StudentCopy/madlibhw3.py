83bc3eaf9ba0ccd7ca84956e880d010028d557c9
Wade P <wadepenman1@0587341414.wireless.umich.net>
2016-11-09 18:35:54 -0500

    New madlib code

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -11,5 +11,40 @@
 # 1) Print the new text
 print("START*******")
 
+import nltk
+from nltk.book import *
+import random
+from nltk import word_tokenize,sent_tokenize
+
+debug = False #True
+
+if debug:
+	print ("Getting information from file madlib_test.txt...\n")
+
+nstr = (text2[:151])
+blk = ' '.join(nstr)
+print (blk)
+tokens = nltk.word_tokenize(blk)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "RB":"an adverb"}
+substitution_probabilities = {"NN":.15,"NNS":.10,"VB":.10,"JJ":.10, "RB":.10}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
 
 print("\n\nEND*******")
